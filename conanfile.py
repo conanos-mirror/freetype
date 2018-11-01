4cf0f6bd0142551e6058d5f058e3d2ca1b5a89bf
cppbitman <chinadaihongjun@gmail.com>
2018-11-01 00:39:31 -0700

    building pango by meson needs freetype-config

--- conanfile.py
+++ conanfile.py
@@ -1,109 +1,53 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from conans import ConanFile, CMake, tools
+from shutil import copyfile
 import os
-
 
 class FreetypeConan(ConanFile):
     name = "freetype"
     version = "2.9.0"
     description = "FreeType is a freely available software library to render fonts."
-    url = "http://github.com/bincrafters/conan-freetype"
+    url = "https://github.com/conanos/freetype"
     homepage = "https://www.freetype.org"
     license = "BSD"
-    author = "Bincrafters <bincrafters@gmail.com>"
-    exports = ["LICENSE.md", "FindFreetype.cmake"]
-    exports_sources = ["CMakeLists.txt", "freetype.pc.in"]
+    exports = ['LICENSE.md']
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
     generators = "cmake"
-    settings = "os", "arch", "compiler", "build_type"
-    options = {
-        "shared": [True, False],
-        "fPIC": [True, False],
-        "with_png": [True, False],
-        "with_zlib": [True, False]
-    }
-    default_options = {'shared': True, 'fPIC': True, 'with_png': True, 'with_zlib': True}
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
+    requires = "zlib/1.2.11@conanos/dev", "bzip2/1.0.6@conanos/dev", "libpng/1.6.34@conanos/dev"
 
-    def requirements(self):
-        if self.options.with_png:
-            self.requires.add("libpng/1.6.34@conanos/dev")
-        if self.options.with_zlib:
-            self.requires.add("bzip2/1.0.6@conanos/dev")
-            self.requires.add("zlib/1.2.11@conanos/dev")
-
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
-
-    def configure(self):
-        del self.settings.compiler.libcxx
-
+    source_subfolder = "source_subfolder"
+    
     def source(self):
         source_url = "https://download.savannah.gnu.org/releases/"
         version = self.version[:-2]
         archive_file = '{0}-{1}.tar.gz'.format(self.name, version)
         source_file = '{0}/{1}/{2}'.format(source_url, self.name, archive_file)
         tools.get(source_file)
-        os.rename('{0}-{1}'.format(self.name, version), self._source_subfolder)
-        self._patch_windows()
-
-    def _patch_windows(self):
-        if self.settings.os == "Windows":
-            pattern = 'if (WIN32 AND NOT MINGW AND BUILD_SHARED_LIBS)\n' + \
-                      '  message(FATAL_ERROR "Building shared libraries on Windows needs MinGW")\n' + \
-                      'endif ()\n'
-            cmake_file = os.path.join(self._source_subfolder, 'CMakeLists.txt')
-            tools.replace_in_file(cmake_file, pattern, '')
-
-    def _patch_msvc_mt(self):
-        if self.settings.os == "Windows" and \
-           self.settings.compiler == "Visual Studio" and \
-           "MT" in self.settings.compiler.runtime:
-            header_file = os.path.join(self._source_subfolder, "include", "freetype", "config", "ftconfig.h")
-            tools.replace_in_file(header_file, "#ifdef _MSC_VER", "#if 0")
-
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        system_libraries = ''
-        if self.settings.os == 'Linux':
-            system_libraries = '-lm'
-        cmake.definitions["PC_SYSTEM_LIBRARIES"] = system_libraries
-        cmake.definitions["PC_FREETYPE_LIBRARY"] = '-lfreetyped' if self.settings.build_type == 'Debug' \
-            else '-lfreetype'
-        if self.options.with_png:
-            cmake.definitions["PC_PNG_LIBRARY"] = '-l%s' % self.deps_cpp_info['libpng'].libs[0]
-        else:
-            cmake.definitions["PC_PNG_LIBRARY"] = ''
-        if self.options.with_zlib:
-            cmake.definitions["PC_ZLIB_LIBRARY"] = '-l%s' % self.deps_cpp_info['zlib'].libs[0]
-            cmake.definitions["PC_BZIP2_LIBRARY"] = '-l%s' % self.deps_cpp_info['bzip2'].libs[0]
-        else:
-            cmake.definitions["PC_ZLIB_LIBRARY"] = ''
-            cmake.definitions["PC_BZIP2_LIBRARY"] = ''
-        cmake.definitions["PROJECT_VERSION"] = self.version
-        cmake.definitions["WITH_ZLIB"] = self.options.with_zlib
-        cmake.definitions["WITH_PNG"] = self.options.with_png
-        cmake.configure(build_dir=self._build_subfolder)
-        return cmake
+        os.rename('{0}-{1}'.format(self.name, version), self.source_subfolder)
 
     def build(self):
-        self._patch_msvc_mt()
-        cmake = self._configure_cmake()
-        cmake.build()
+        with tools.chdir(self.source_subfolder):
+            with tools.environment_append(
+                {'PKG_CONFIG_PATH': "%s/lib/pkgconfig:%s/lib/pkgconfig:%s/lib/pkgconfig"
+                %(self.deps_cpp_info["zlib"].rootpath,
+                self.deps_cpp_info["bzip2"].rootpath,
+                self.deps_cpp_info["libpng"].rootpath)}):
+
+                _args = ['--prefix=%s/builddir'%(os.getcwd()), '--libdir=%s/builddir/lib'%(os.getcwd()), '--with-harfbuzz=no']
+                if self.options.shared:
+                    _args.extend(['--enable-shared=yes','--enable-static=no'])
+                else:
+                    _args.extend(['--enable-shared=no','--enable-static=yes'])
+                self.run('./configure %s'%(' '.join(_args)))#space
+                self.run('make -j2')
+                self.run('make install')
 
     def package(self):
-        cmake = self._configure_cmake()
-        cmake.install()
-        self.copy("FindFreetype.cmake")
-        self.copy("FTL.TXT", dst="licenses", src=os.path.join(self._source_subfolder, "docs"))
-        self.copy("GPLv2.TXT", dst="licenses", src=os.path.join(self._source_subfolder, "docs"))
-        self.copy("LICENSE.TXT", dst="licenses", src=os.path.join(self._source_subfolder, "docs"))
+        if tools.os_info.is_linux:
+            with tools.chdir(self.source_subfolder):
+                self.copy("*", src="%s/builddir"%(os.getcwd()))
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-        if self.settings.os == "Linux":
-            self.cpp_info.libs.append("m")
-        self.cpp_info.includedirs.append(os.path.join("include", "freetype2"))
+
